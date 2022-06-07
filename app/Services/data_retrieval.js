530c4fb9b68763dc02a856abb05ea7ba46fb89db
aggron2002 <arun.physics500@gmail.com>
2022-06-07 11:14:49 +0530

    component created

--- app/Services/data_retrieval.js
+++ app/Services/data_retrieval.js
@@ -2,7 +2,7 @@ angular.module('dataRetrieval',[])
 .service('customHttpService',['$http',function($http){
 	this.handleCheck=function(handle,callBack){
 		let url = 'https://codeforces.com/api/user.info?handles='+handle;
-		$http.get(url)
+		let promise=$http.get(url)
 		.then(function (response){
 			if(response.status==200)
 			{
@@ -14,5 +14,21 @@ angular.module('dataRetrieval',[])
 					callBack("");
 			}
 		});
+		return promise;
 	};
+	this.dataGather=function(url,callBack){
+		let promise=$http.get(url)
+		.then(function (response){
+			if(response.status==200)
+			{
+				callBack(response.data);
+			}
+		}, function (response){
+			if(response.status==400 || response.status==404)
+			{
+					callBack("");
+			}
+		});
+		return promise;
+	}
 }]);
